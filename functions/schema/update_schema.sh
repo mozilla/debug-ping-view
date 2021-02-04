c4d63784fcac0c7a578a754a734b4c406133a148
Arkadiusz Komarzewski <akomarzewski@mozilla.com>
2021-02-04 15:20:31 +0100

    Update mozilla-pipeline-schemas branch name to main in helper script

--- functions/schema/update_schema.sh
+++ functions/schema/update_schema.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 BASEDIR=$(dirname $0)
-wget https://raw.githubusercontent.com/mozilla-services/mozilla-pipeline-schemas/master/schemas/glean/glean/glean.1.schema.json -O $BASEDIR/glean.1.schema.json
+wget https://raw.githubusercontent.com/mozilla-services/mozilla-pipeline-schemas/main/schemas/glean/glean/glean.1.schema.json -O $BASEDIR/glean.1.schema.json
