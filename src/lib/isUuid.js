5be1a09515492c9b34ee4d5d2e84d6f6a9b440a6
Bruno Rosa <rosahbruno@gmail.com>
2023-01-26 11:05:04 -0500

    Required items for open sourcing
    added
    - CODE_OF_CONDUCT
    - LICENSE
    - License headers at the top of each code file

--- src/lib/isUuid.js
+++ src/lib/isUuid.js
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const regexExp =
   /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
 
