35ebf4c14d4bfdb83cd2d7b1b14b5df40971d7a9
Abhishek Aggarwal <aaggarwal@mozilla.com>
2023-09-15 15:38:29 +0200

    Call Glean.setUploadEnabled unconditionally on page route

--- src/lib/telemetry.js
+++ src/lib/telemetry.js
@@ -7,7 +7,6 @@
 import Glean from '@mozilla/glean/web';
 
 const APP_NAME = 'debug-ping-view';
-const APP_TELEMETRY_PREFERENCE_STORAGE_KEY = 'telemetryEnabled';
 
 /**
  * Check browser's `navigator.doNotTrack` property to determine the
@@ -20,42 +19,16 @@ function isTelemetryEnabled() {
 }
 
 /**
- * Set the current telemetry preference in `window.sessionStorage` object.
- *
- * @param {string} telemetryPreferenceValue a string (either 'true' or 'false')
- *                                          representing telemetry preference
- */
-function setAppTelemetryPreferenceInStorage(telemetryPreferenceValue) {
-  sessionStorage.setItem(APP_TELEMETRY_PREFERENCE_STORAGE_KEY, telemetryPreferenceValue);
-}
-
-/**
- * Get the current telemetry preference from `window.sessionStorage` object.
- *
- * @returns {string} a string (either 'true' or 'false')
- */
-function getAppTelemetryPreferenceFromStorage() {
-  return sessionStorage.getItem(APP_TELEMETRY_PREFERENCE_STORAGE_KEY);
-}
-
-/**
  * Initialize telemetry client, setting it's upload status based on the current
  * telemetry preference.
  */
 export function initTelemetryClient() {
-  const telemetryEnabled = isTelemetryEnabled();
-  setAppTelemetryPreferenceInStorage(telemetryEnabled.toString());
-  Glean.initialize(APP_NAME, telemetryEnabled, { maxEvents: 1 });
+  Glean.initialize(APP_NAME, isTelemetryEnabled(), { maxEvents: 1 });
 }
 
 /**
- * Update telemetry client's upload status if telemetry preference changed.
+ * Update telemetry client's upload status.
  */
 export function updateTelemetryClientUploadStatus() {
-  const appTelemetryPreference = getAppTelemetryPreferenceFromStorage();
-  const telemetryEnabled = isTelemetryEnabled();
-  if (appTelemetryPreference !== telemetryEnabled.toString()) {
-    setAppTelemetryPreferenceInStorage(telemetryEnabled.toString());
-    Glean.setUploadEnabled(telemetryEnabled);
-  }
+  Glean.setUploadEnabled(isTelemetryEnabled());
 }
