9226b30bea0fc05e098f561778ec8f7d4e207beb
Bruno Rosa <rosahbruno@gmail.com>
2023-11-01 13:52:37 -0400

    Only enable telemetry in prod

--- src/lib/telemetry.js
+++ src/lib/telemetry.js
@@ -16,6 +16,11 @@ const APP_NAME = 'debug-ping-view';
  * @returns {boolean} true if telemetry can be collected, else false.
  */
 function isTelemetryEnabled() {
+  // If the app is in development mode, we don't want to initialize Glean.
+  if (process.env.NODE_ENV === 'development') {
+    return false;
+  }
+
   return navigator.doNotTrack !== '1';
 }
 
