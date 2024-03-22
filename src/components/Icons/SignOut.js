f37c148ff96158151083897b8bef48f896dd8ddd
Abhishek Aggarwal <aaggarwal@mozilla.com>
2024-03-22 21:28:38 +0100

    Remove pointerEvents='none' instrumentation from nested elements
     - Improved glean click events for nested elements allow us to
       remove this instrumentation

--- src/components/Icons/SignOut.js
+++ src/components/Icons/SignOut.js
@@ -15,7 +15,6 @@ const SignOutIcon = () => {
       fill='currentColor'
       className='bi bi-box-arrow-right'
       viewBox='0 0 16 16'
-      pointerEvents='none'
     >
       <path
         fillRule='evenodd'
