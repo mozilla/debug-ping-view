aa112ffdefd0347621a3c60bf1e2bed9adc4437d
Chris H-C <chutten@mozilla.com>
2026-04-09 15:17:02 -0400

    bug 1970648 - Don't use rich plots on pings with 'too many' richly-plottable metrics

--- src/components/Metrics/index.js
+++ src/components/Metrics/index.js
@@ -4,14 +4,28 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import React from 'react';
+import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import BarChart from './components/BarChart';
 import Histogram from './components/Histogram';
 import MetricValuesTable from './components/MetricValuesTable';
 
+// A given "metrics" ping may have hundreds of plotable metrics.
+// That's too many to render quickly, so fall back to the quick
+// MetricsValueTable if we have "too many".
+// ("too many" defined here as "whatever seemed right at the time").
+const MAX_DIST_PLOTS = 50;
+const MAX_LABELED_COUNTER_PLOTS = 50;
+
 const Metrics = ({ metrics }) => {
+  /// handlers ///
+  const [showAnyway, setShowAnyway] = useState(false);
+
+  const handleShowAnyway = () => {
+    setShowAnyway(true);
+  }
+
   /// helpers ///
   /**
    * Render the correct visualization for each metric type. Most metrics use
@@ -28,19 +42,51 @@ const Metrics = ({ metrics }) => {
       return null;
     }
 
-    let content;
+    let content, advisory;
     switch (metricKey) {
       case 'timing_distribution':
       case 'memory_distribution':
       case 'custom_distribution':
-        content = Object.keys(metricData).sort().map((key, i) => (
-          <Histogram key={`${key}${i}`} title={key} data={metricData[key]} />
-        ));
+        if (!showAnyway && Object.keys(metricData).length > MAX_DIST_PLOTS) {
+          advisory = (
+            <p>
+              <strong>Advisory: </strong>&nbsp;
+              The number of {metricKey} metrics is more than the threshold
+              for rich plot display ({Object.keys(metricData).length} / {MAX_DIST_PLOTS}).
+              Raw metric values table display is being used instead.
+              <br/>
+              <button className='btn btn-sm btn-outline-secondary' onClick={handleShowAnyway}>
+                Render all rich plots anyway (may take tens of seconds)
+              </button>
+            </p>
+          );
+          content = <MetricValuesTable metricData={metricData} />;
+        } else {
+          content = Object.keys(metricData).sort().map((key, i) => (
+            <Histogram key={`${key}${i}`} title={key} data={metricData[key]} />
+          ));
+        }
         break;
       case 'labeled_counter':
-        content = Object.keys(metricData).sort().map((key, i) => (
-          <BarChart key={`${key}${i}`} title={key} data={metricData[key]} />
-        ));
+        if (!showAnyway && Object.keys(metricData).length > MAX_LABELED_COUNTER_PLOTS) {
+          advisory = (
+            <p>
+              <strong>Advisory: </strong>&nbsp;
+              The number of {metricKey} metrics is more than the threshold
+              for rich plot display ({Object.keys(metricData).length} / {MAX_LABELED_COUNTER_PLOTS}).
+              Raw metric values table display is being used instead.
+              <br/>
+              <button className='btn btn-sm btn-outline-secondary' onClick={handleShowAnyway}>
+                Render all rich plots anyway (may take tens of seconds)
+              </button>
+            </p>
+          );
+          content = <MetricValuesTable metricData={metricData} />;
+        } else {
+          content = Object.keys(metricData).sort().map((key, i) => (
+            <BarChart key={`${key}${i}`} title={key} data={metricData[key]} />
+          ));
+        }
         break;
       default:
         content = <MetricValuesTable metricData={metricData} />;
@@ -52,6 +98,7 @@ const Metrics = ({ metrics }) => {
         <p>
           <strong>{metricKey}</strong>
         </p>
+        {advisory}
         {content}
       </div>
     );
